8f6845493b66f99c265c3ee13410f339c3f8a883
CHELSEAROYER <chelsearoyer1990@gmail.com>
2015-08-11 14:31:52 -0500

    Create Dano_Chelsea_Expressions_Assignment.js
    God help my arteries. Cardiac arrest, here I come!

---
+++ Dano_Chelsea_Expressions_Assignment.js
@@ -0,0 +1,10 @@
+//Time to take a look at how much money I throw at the takeout industry. Totals of money spent on food within a two week period.
+var mcdonalds = 5;
+var applebees = 11;
+var tacoBell = 4;
+var gasStation = 6;
+var coffee = 5;
+var burgerKing = 9;
+var pizzaHut = 15;
+var wendys = 8;
+var takeOut = [mcdonalds, applebees, tacoBell, gasStation, coffee, burgerKing, pizzaHut, wendys]
