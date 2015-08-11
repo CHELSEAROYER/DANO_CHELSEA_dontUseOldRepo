aac8c401c4cea2b52d0818b823d8a9756f44128a
CHELSEAROYER <chelsearoyer1990@gmail.com>
2015-08-11 11:46:25 -0500

    Create Expressions.js

---
+++ Expressions.js
@@ -0,0 +1,83 @@
+var a = 4;
+console.log(a);
+
+
+var a = 4;
+a = a+3
+console.log (a);
+
+
+
+var yearBorn = 1990;
+2015-yearBorn;
+var age = 2015 - yearBorn;
+console.log (age);
+
+//area of the triangle is half the width times the height.
+var width = 5;
+var Height = 8/2;
+var area = width * height;
+console.log(area);
+
+
+
+//Using Modulo...
+//You can use this to find the remainder of division.
+
+var remainder = 32/10;
+console.log (remainder);
+//the practical use for this is to find out if it's an odd or an even number.
+
+Var	remainder = 32%10;
+console.log (remainder);
+
+
+
+//ORDER OF OPERATIONS
+
+var quiz1 =87;
+var quiz2 =100;
+var quiz3 =123;
+var quiz4 =678;
+//the average of all the quizzes are found by adding the values together, and then dividing them by the number of separate parts.
+
+//you can use-parentheses
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+
+
+//USING ARRAYS IN EXPRESSIONS.
+var orangeBinsnge = [234, 567, 883]
+vara total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total); 
+
+//CONCATENATING
+//essentially combining strings
+var firstName = "Chelsea";
+var lastName = "Dano";
+var FullName = firstName + lastName;
+console.log(fullName);
+
+//to add a space in between fn & ln
+var firstName = "Chelsea";
+var lastName = "Dano";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//concatenating strings
+var a = "6";
+var b = "7";
+var result = a+b;
+console.log(result);
+//creates 67, holds the characters, but does not add them...
+
+//CASTING
+var stringVar = "6";
+var result = 7 + stringVar; 
+console.log(result);
+//tell the computer to treat stringVar like a numerical value
+
+var areaCode = 816;
+var firstPart = 854;
+var secondPart = 9390; 
+var phoneNo = string(areaCode)+"-"+string(firstPart)+"-"+string(secondPart);
+console.log(phoneNo);
