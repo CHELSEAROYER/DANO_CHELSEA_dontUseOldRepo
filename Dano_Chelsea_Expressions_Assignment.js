b777ddb481b1f0368bfa3652fa426056bd7d6001
CHELSEAROYER <chelsearoyer1990@gmail.com>
2015-08-11 22:13:56 -0500

    Update Dano_Chelsea_Expressions_Assignment.js
    grammatical correction

--- Dano_Chelsea_Expressions_Assignment.js
+++ Dano_Chelsea_Expressions_Assignment.js
@@ -1,6 +1,6 @@
 //Chelsea Dano. 08-11-2015. 
 
-//I always watch the clock very closely when I'm at work and calculate how much time is left for fun, so
+//I always watch the clock very closely when I'm at work to calculate how much time is left for fun, so
 //for this assignment I wanted to create a calculator that would work that out for me!
 
 var location = prompt ("Are you at work today?");
