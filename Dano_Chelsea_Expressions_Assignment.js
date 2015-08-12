8c37bcc4b7970ef27183c1ab3184ae7ef60d4070
CHELSEAROYER <chelsearoyer1990@gmail.com>
2015-08-11 21:30:07 -0500

    Create Dano_Chelsea_Expressions_Assignment.js

---
+++ Dano_Chelsea_Expressions_Assignment.js
@@ -0,0 +1,10 @@
+//Chelsea Dano. 08-11-2015. 
+
+//I always watch the clock very closely when I'm at work and calculate how much time is left for fun, so
+//for this assignment I wanted to create a calculator that would work that out for me!
+
+var location = prompt ("Are you at work today?");
+var time = prompt ("What time is it right now?");
+var letsGo = prompt ("When are you scheduled to leave work?);
+var timeLeft = letsGo-time;
+var result = ("You have"+ timeLeft +"left before it's time to go home!");
